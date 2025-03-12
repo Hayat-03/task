9ab0efe71b0c908808d28270c1169de30b37aee8
Hayat-03 <malikhayat54@gmail.com>
2025-03-12 23:25:03 +0500

    chnage

--- playwright.config.js
+++ playwright.config.js
@@ -3,7 +3,7 @@ const { defineConfig, devices } = require('@playwright/test');
 
 module.exports = defineConfig({
   timeout: 60000,
-  testDir: './tests',
+  testDir: '../tests',
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
